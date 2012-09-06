5d8cb1af30dc348b89a0dfc4a00ef6ab0b466d66
Andrew Hsieh <andrewhsieh@google.com>
2012-09-06 09:35:54 +0800

    Fix MIPS GCC __builtin_unreachable issue compiling LLVM
    See Bug 54369:
      http://gcc.gnu.org/bugzilla/show_bug.cgi?id=54369
    
    Change-Id: I1a84f7c95e05e26b2906e79947a43f64d5621362

--- gcc-4.4.3/gcc/config/mips/mips.c
+++ gcc-4.4.3/gcc/config/mips/mips.c
@@ -13345,7 +13345,10 @@ mips_reorg (void)
   if (mips_r10k_cache_barrier != R10K_CACHE_BARRIER_NONE)
     r10k_insert_cache_barriers ();
   if (optimize > 0 && flag_delayed_branch)
-    dbr_schedule (get_insns ());
+    {
+      cleanup_barriers ();
+      dbr_schedule (get_insns ());
+    }
   mips_reorg_process_insns ();
   if (!TARGET_MIPS16
       && TARGET_EXPLICIT_RELOCS
